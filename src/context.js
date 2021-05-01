43a45cddc88265af7896d984e01f98e7035d495c
tyler <tyler@cruftbusters.com>
2021-05-01 00:10:51 -0500

    convert context state to reducer

--- src/context.js
+++ src/context.js
@@ -1,21 +1,41 @@
-import { useEffect, useState, createContext } from 'react'
+import { useEffect, createContext, useReducer } from 'react'
 
 const context = createContext()
 
-function ContextProvider({ children }) {
-  const [neighborhoods, setNeighborhoods] = useState({
+const initialState = {
+  area: {
     features: [],
     type: 'FeatureCollection',
-  })
+  },
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'newArea':
+      return {
+        ...state,
+        area: action.payload,
+      }
+    default:
+      throw Error(`irreducable action type ${action.type}`)
+  }
+}
+
+function ContextProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { area } = state
   useEffect(() => {
     fetch('/vectors/minneapolis.json')
       .then((response) => response.json())
-      .then((_neighborhoods) => setNeighborhoods(_neighborhoods))
-      .catch((error) =>
-        console.log('Failed to fetch neighborhoods', error),
-      )
+      .then((area) => dispatch({ type: 'newArea', payload: area }))
+      .catch((error) => console.log('Failed to fetch area', error))
   }, [])
-  return <context.Provider value={{ neighborhoods }} children={children} />
+  return (
+    <context.Provider
+      value={{ neighborhoods: area }}
+      children={children}
+    />
+  )
 }
 
 export default context
